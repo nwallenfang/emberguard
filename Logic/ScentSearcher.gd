0af7cbb839390852a1a95f21d4d30c5e14d1cf3d
nheeb <nitai64@yahoo.de>
2022-03-10 13:56:32 +0100

    Dust tweaks

--- Logic/ScentSearcher.gd
+++ Logic/ScentSearcher.gd
@@ -10,7 +10,7 @@ func get_target_position() -> Vector3:
 	if not is_instance_valid(scent_emitter):
 		scent_emitter = Game.player.scent_emitter
 	for scent in scent_emitter.scents:
-		print(scent)
+		#print(scent)
 		cast_to = to_local(scent)
 		force_raycast_update()
 		if not is_colliding():

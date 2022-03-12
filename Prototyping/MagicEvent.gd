6aaab47880b037207279b27ed52c25df46c4f479
nheeb <nitai64@yahoo.de>
2022-03-12 22:20:51 +0100

    Cutscene

---
+++ Prototyping/MagicEvent.gd
@@ -0,0 +1,16 @@
+extends Spatial
+
+export var beam_shoot := 0.0 setget set_beam_shoot
+
+func set_beam_shoot(x):
+	beam_shoot = x
+	$Laser/MeshInstance.material_override.set("shader_param/shoot_offset", x)
+
+func start_cutscene():
+	pass
+
+func cut_scene_wagon_hit():
+	pass
+
+func cutscene_done():
+	pass
